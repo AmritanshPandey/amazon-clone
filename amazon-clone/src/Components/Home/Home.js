c5f970e3759b1beea659b99772cbb41fe04352a7
Amritansh Pandey <amritanshpandey@Amritanshs-MacBook-Air.local>
2020-10-02 21:20:29 +0530

    home-product-grid

--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Home.css";
 
+import Product from "./Product";
+
 function Home() {
     return (
         <div className="home">
@@ -8,6 +10,26 @@ function Home() {
          className="home-backgroundImage"
          src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg"
          alt="bg" />
+
+
+                <div className="home-container">
+                    <div className="home-row">
+                        <Product 
+                        id="1235353"
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
+                        price={615}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/81jgCiNJPUL._AC_UY218_.jpg"
+                        />
+                        <Product 
+                        id="1235353"
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
+                        price={615}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/81jgCiNJPUL._AC_UY218_.jpg"
+                        />
+                </div>
+            </div>
         </div>
     )
 }
